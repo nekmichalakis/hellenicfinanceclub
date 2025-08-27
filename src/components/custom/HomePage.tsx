89e0c931fd985d41ae349dc2b9ada84937754a15
nekmichalakis <nekmichalakis@gmail.com>
2025-08-27 17:52:32 +0300

    feat: font

--- src/components/custom/HomePage.tsx
+++ src/components/custom/HomePage.tsx
@@ -8,10 +8,10 @@ const HomePage = () => {
       <Hero />
 
       <div className="flex flex-col py-16 gap-6 items-center bg-secondary">
-        <h1 className={"text-4xl md:text-6xl font-bold"}>
+        <h1 className={"text-4xl md:text-6xl font-light"}>
           Explore Our Scope Of Work
         </h1>
-        <p className={"text-xl"}>Our Activities & Services</p>
+        <p className={"text-xl font-[200]"}>Our Activities & Services</p>
         <Button className={"rounded-none py-6 px-10 mt-12 text-md font-light"}>
           Learn More
         </Button>
@@ -19,8 +19,8 @@ const HomePage = () => {
 
       <div className="flex bg-white">
         <div className={"flex flex-col gap-6 p-16 w-1/2"}>
-          <h1 className={"text-4xl md:text-6xl font-bold"}>Get to Know Us</h1>
-          <p className={"text-xl"}>Hellenic Finance Network</p>
+          <h1 className={"text-4xl md:text-6xl font-light"}>Get to Know Us</h1>
+          <p className={"text-xl font-[200]"}>Hellenic Finance Network</p>
           <Button
             className={
               "rounded-none py-6 px-10 mt-12 text-md font-light self-center"
@@ -40,7 +40,7 @@ const HomePage = () => {
       </div>
 
       <div className="flex gap-16 bg-primary px-12 py-16 items-center justify-center">
-        <h1 className={"text-6xl text-white"}>Admits Include</h1>
+        <h1 className={"text-6xl text-white font-light"}>Admits Include</h1>
         <div className={"size-36 bg-black flex items-center justify-center"}>
           <h1 className={"text-gray-400 text-6xl"}>A</h1>
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/fraunces/200.css";
+import "@fontsource/fraunces/300.css";
+import "@fontsource/fraunces/400.css";
+import "@fontsource/fraunces/500.css";
+import "@fontsource/fraunces/600.css";
+import "@fontsource/fraunces/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
